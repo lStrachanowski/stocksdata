e8a773a0749634add5802c64c3bc668def6c6713
łukasz Strachanowski <lukasz.strachanowski@gmail.com>
2019-06-14 21:41:01 +0200

    Database auto update added

--- modules/database.py
+++ modules/database.py
@@ -2,8 +2,8 @@ import sys
 import os
 import requests
 import zipfile
-from datetime import datetime
-from sqlalchemy import create_engine, MetaData, Table, Column,String,Numeric,Integer,Time,Date, select,exists, func, and_
+import datetime
+from sqlalchemy import create_engine, MetaData, Table, Column,String,Numeric,Integer,Time,Date, select,exists, func, and_, text,Float,desc
 sys.path.append(os.getcwd()+'\\modules\\')
 import credentials 
 # zawiera login i hasło do bazy danych 
@@ -11,7 +11,6 @@ import csv
 
 engine = create_engine("postgresql://postgres:"+credentials.PASSWORD + "@localhost/" + credentials.DATABASE_NAME)
 meta = MetaData()
-directory = os.getcwd()+'\\data\\'
 # Towrzy tabele stocks
 stocks = Table(
     "stocks", meta,
@@ -23,31 +22,51 @@ stocks = Table(
 # Tworzy tabelę z podsumowaniem transakcji dnia
 day_transactions = Table(
     'day_transactions', meta, 
-    Column("TICKER", String),
+    Column("NAME", String),
     Column("DATE", Date),
     Column("OPEN",Numeric),
     Column("HIGH",Numeric),
     Column("LOW",Numeric),
     Column("CLOSE",Numeric),
-    Column("VOLUME",Numeric)
+    Column("VOLUME",Float )
 )
 
 # Tworzy tabele z transakcjami w ciągu dnia
 details_day_transactions = Table(
     'details_day_transactions', meta, 
-    Column("TICKER", String),
+    Column("NAME", String),
     Column("DATE", Date),
     Column("HOUR",Time),
     Column("OPEN",Numeric),
     Column("HIGH",Numeric),
     Column("LOW",Numeric),
     Column("CLOSE",Numeric),
-    Column("VOLUME",Numeric)
+    Column("VOLUME",Numeric )
 )
 
 # Inicjuje tabele w bazie danych
-meta.create_all(engine)
+# meta.create_all(engine)
 
+# Usuwa / tworzy tabele z bazy danych 
+# d-drop
+# c-create
+def table_operations(table_name, operation):
+    if table_name == 'stocks':
+        if operation == 'd':
+            stocks.drop(engine)
+        if operation == 'c':
+            stocks.create(engine)
+    elif table_name == 'daytransactions':
+        if operation == 'd':
+            day_transactions.drop(engine)
+        if operation == 'c':
+            day_transactions.create(engine)
+    else:
+        if operation == 'd':
+            details_day_transactions.drop(engine)
+        if operation == 'c':
+            details_day_transactions.create(engine)
+        
 # Metoda, która pobiera dane walorów z pliku csv i dodaje je do bazy danych
 def load_stock_data():
     with open(os.getcwd()+'\\stock_tickers.csv' ,newline='', mode='r') as csv_file:
@@ -59,8 +78,25 @@ def load_stock_data():
         conn.execute(stocks.insert(), stocks_data)
         conn.close()
 
+# Pobiera z bazy dane o walorze z podanego okresu
+def get_data_from_db(from_date,to_date,stock_name):
+    selected = day_transactions.select().where(and_(day_transactions.c.DATE >= from_date , day_transactions.c.DATE <= to_date, day_transactions.c.NAME == stock_name.upper())).order_by(day_transactions.c.DATE)
+    conn = engine.connect()
+    data = conn.execute(selected)
+    conn.close()
+    return data 
+
+# Sprawdza datę ostatniego wpisu w bazie danych 
+def check_last_entry():
+    selected = day_transactions.select().where(day_transactions.c.NAME == 'WIG20').order_by(desc(day_transactions.c.DATE)).limit(1)
+    conn = engine.connect()
+    data = conn.execute(selected)
+    conn.close()
+    return data
+
 # Pobiera dane z plików i inicjuje je w bazie danych
 def load_stocks_details():
+    directory = os.getcwd()+'\\data\\'
     for file in os.listdir(directory):
         print(file)
         with open(directory+file,newline='', mode="r" ) as csv_file:
@@ -69,17 +105,32 @@ def load_stocks_details():
             itercsv = iter(csv_data)
             next(itercsv)
             for row in itercsv:
-                datetime_object = datetime.strptime(row[1],"%Y%m%d")
+                datetime_object = datetime.datetime.strptime(row[1],"%Y%m%d")
                 name = row[0]
                 stock_open = row[2]
                 stock_high = row[3]
                 stock_low = row[4]
                 stock_close = row[5]
                 stock_volume = row[6]
-                stocks_data.append({"TICKER":name,"DATE":datetime_object, "OPEN": float(stock_open),"HIGH": float(stock_high), "LOW": float(stock_close), "CLOSE": float(stock_low), "VOLUME": int(stock_volume)  })
+                stocks_data.append({"NAME":name,"DATE":datetime_object, "OPEN": float(stock_open),"HIGH": float(stock_high), "LOW": float(stock_close), "CLOSE": float(stock_low), "VOLUME": float(stock_volume)  })
             conn = engine.connect()
             conn.execute(day_transactions.insert(), stocks_data)
             conn.close()
+
+#Dodaje pozycje o walorze do bazy danych
+def add_stock_data(stock):
+    stocks_data = []
+    datetime_object = datetime.strptime(stock[1],"%Y%m%d")
+    name = stock[0]
+    stock_open = stock[2]
+    stock_high = stock[3]
+    stock_low = stock[4]
+    stock_close = stock[5]
+    stock_volume = stock[6]
+    stocks_data.append({"NAME":name,"DATE":datetime_object, "OPEN": float(stock_open),"HIGH": float(stock_high), "LOW": float(stock_close), "CLOSE": float(stock_low), "VOLUME": float(stock_volume)  })
+    conn = engine.connect()
+    conn.execute(day_transactions.insert(), stocks_data)
+    conn.close()
 
 # Pobiera wartości z bazy danych 
 def get_data(value):
@@ -125,43 +176,89 @@ def download_data():
         print("Something went wrong with dowloading NewConnect data.")
 
 # Wypakowuje dane z pliku zip I usuwa zbędne pliki      
-def unzip_file():
-    gpw_file = 'mstall.zip'
-    newconnect_file = 'mstncn.zip'
+def unzip_file(directory, file_list):
     try:
-        print('Unzipping GPW file.')
-        with zipfile.ZipFile(directory + gpw_file) as myzip:
-            myzip.extractall(directory)
-        print('GPW files unziped.')
-        if os.path.isfile(directory + gpw_file):
-            os.remove(directory + gpw_file) 
-        print('Unzipping Newconnect file.')
-        with zipfile.ZipFile(directory + newconnect_file) as myzip_newconnect:
-            myzip_newconnect.extractall(directory)
-        print('Newconnect files unziped.')
-        if os.path.isfile(directory + newconnect_file):
-            os.remove(directory + newconnect_file) 
-
-        # Usuwa pliki ze starymi akcjami  
-        print('Start to delete old files')
-        with open('oldstocks.csv', newline='') as file:
-            reader = csv.reader(file, delimiter=' ')
-            for item in list(reader)[:-1]:
-                file_path = directory + item[0]
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-        print('Old files deleted')
-        print('Start to delete old files')
-        for file in os.listdir(directory):
-            conn = engine.connect()
-            check = exists(select([stocks]).where(stocks.c.NAME == file[:-4])).select()
-            pr = conn.execute(check)
-            conn.close()
-            if pr.first()[0] == False:
-                if os.path.isfile(directory + file):
-                    os.remove(directory + file)
-        print('Old files deleted')
+        for item in file_list:
+            print('Unzipping ' + item + ' file.')
+            with zipfile.ZipFile(directory + item) as myzip:
+                myzip.extractall(directory)
+            print(item + ' files unziped.')
+            if os.path.isfile(directory + item):
+                os.remove(directory + item ) 
     except:
         print("Something went wrong with file unzipping")
 
+# Usuwa pliki ze starymi akcjami 
+def delete_old_files():
+    directory = os.getcwd()+'\\data\\'
+    print('Start to delete old files')
+    with open('oldstocks.csv', newline='') as file:
+        reader = csv.reader(file, delimiter=' ')
+        for item in list(reader)[:-1]:
+            file_path = directory + item[0]
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+    print('Old files deleted')
+    print('Start to delete old files')
+    for file in os.listdir(directory):
+        conn = engine.connect()
+        check = exists(select([stocks]).where(stocks.c.NAME == file[:-4])).select()
+        pr = conn.execute(check)
+        conn.close()
+        if pr.first()[0] == False:
+            if os.path.isfile(directory + file):
+                os.remove(directory + file)
+    print('Old files deleted')    
 
+
+# Pobiera aktualne dane
+def download_week():
+    url = "https://info.bossa.pl/pub/metastock/mstock/sesjaall/few_last.zip"
+    path = os.getcwd()+'\\temp\\'
+    try:
+        r = requests.get(url)
+        with open(path + "gpw.zip", "wb" ) as code:
+            code.write(r.content)
+    except:
+        print("Something went wrong with dowloading GPW data.")
+        
+# Sprawdza czy plik z danym walorem jest w bazie danych
+def check_stock(stock):
+    conn = engine.connect()
+    check = exists(select([stocks]).where(stocks.c.NAME == stock)).select()
+    pr = conn.execute(check)
+    conn.close()
+    return pr.first()[0]
+
+# Aktualizuje bazę danych w zalezności od tego , kiedy miała miejsce ostatnia aktualizacja
+def update_db(): 
+    today_date = datetime.date.today()
+    last_date = list(check_last_entry())[0][1]
+    diff = today_date - last_date
+    if diff.days < 7:
+        download_week()
+        unzip_file(os.getcwd()+'\\temp\\',['gpw.zip'])
+        file_list = os.listdir(os.getcwd()+'\\temp\\')
+        for file in file_list:
+            s = file.split(".")[0]
+            if s[0] == '2':
+                file_time = datetime.datetime.strptime(s, '%Y%m%d' )
+                db_last_time = datetime.datetime(last_date.year, last_date.month, last_date.day)
+                t_diff = file_time - db_last_time
+                if t_diff.days > 0 :
+                    print("do dodania " + file)
+                    with open(os.getcwd()+'\\temp\\'+file, newline='') as prn_file:
+                        reader = csv.reader(prn_file, delimiter=',')
+                        for r in reader:
+                            if check_stock(r[0]):
+                                add_stock_data(r)
+        print('Baza aktualna')
+    else:
+        print('Odświeżanie bazy danych')
+        table_operations('daytransactions','d')
+        table_operations('daytransactions','c')
+        download_data()
+        unzip_file(os.getcwd()+'\\data\\',['mstall.zip','mstncn.zip'])
+        delete_old_files()
+        load_stocks_details()
+        print('Baza aktualna')
--- stocks.py
+++ stocks.py
@@ -3,6 +3,7 @@ import os
 # Ścieżka dla importu modułów
 sys.path.append(os.getcwd()+'\\modules\\')
 import database
+import csv
 
 # Ładuje dane z csv do bazy danych
 # database.load_stock_data()
@@ -11,7 +12,21 @@ import database
 # database.download_data()
 
 # Wypakowuje dane
-# database.unzip_file()
+# database.unzip_file(os.getcwd()+'\\data\\',['mstall.zip','mstncn.zip'])
+
+#Kasuje niepotrzebne pliki
+# database.delete_old_files()
 
 # Inicjuje dane o dziennych zmianach w bazie danych 
 # database.load_stocks_details()
+
+
+# res = database.get_data_from_db('20180501','20190603','11bit')
+# for r in res:
+#     print(r)
+
+
+
+
+
+database.update_db()
